d83c0513b6eecb488df88133c605ef22576d788e
Alexsander Oliveira <alexsander1996@yahoo.com.br>
2020-12-13 15:22:25 -0300

    Upload Parser e Main
    Envio da classe Parser e Main de acordo com exemplo do sor, fornecido em aula.

--- Compiler/src/main/java/br/com/uniritter/compiler/Main.java
+++ Compiler/src/main/java/br/com/uniritter/compiler/Main.java
@@ -1,16 +1,49 @@
 package br.com.uniritter.compiler;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Alexsander Oliveira
  * @author Tailine Wornath
  * @author Teo Falleiro
- * 
+ *
  */
 public class Main {
-    
-    public static void main(String[] args) {
-        
+
+    public static void testaLexico() {
+        //Cria lista de tokens
+        HashMap<String, Token> lt = new HashMap<>();
+        HashMap<String, Token> lt2 = new HashMap<>();
+
+
+        //Cria analisador lexico
+        Lexico lexico = new Lexico("teste1.lpd");
+        Lexico lexico2 = new Lexico("teste2.lpd");
+
+        //Realiza a analise lexica do codigo
+        lt = lexico.listaTokens();
+        lt2 = lexico.listaTokens();
     }
-    
+
+    public static void testaParser() {
+        Lexico lexico = new Lexico("teste2.lpd");
+        Parser parser = new Parser(lexico);
+        parser.parse();
+
+    }
+
+//    public static void testaParserCodigo() {
+//        Lexico lexico = new Lexico("teste2.lpd");
+//        ParserCodigo parser = new ParserCodigo(lexico);
+//        String codigo = parser.parse();
+//        System.out.println("Codigo: \n" + codigo);
+//    }
+
+    public static void main(String[] args) {
+        testaLexico();
+        //testaParser();
+        //testaParserCodigo();
+    }
+
 }
--- Compiler/src/main/java/br/com/uniritter/compiler/Parser.java
+++ Compiler/src/main/java/br/com/uniritter/compiler/Parser.java
@@ -3,8 +3,64 @@ package br.com.uniritter.compiler;
 /**
  *
  * @author Alexsander Oliveira
- * 
+ *
  */
-public abstract class Parser {
-    
+public class Parser {
+
+    Lexico lexico;
+    Token token;
+
+    //Valor da express�o
+    int valor;
+
+    public Parser(Lexico lex) {
+        lexico = lex;
+    }
+
+    public void Erro(String s) {
+        System.out.print(s);
+    }
+
+    public Token bt() {
+        Token t = lexico.buscaToken();
+        System.out.println(t.toString());
+        return t;
+    }
+
+    public void parse() {
+        expressao();
+    }
+
+    public void expressao() {
+        token = bt();
+        if (token.tipoToken == TipoToken.SMAIS || token.tipoToken == TipoToken.SMENOS) {
+            token = bt();
+        }
+        termo();
+        while (token.tipoToken == TipoToken.SMAIS || token.tipoToken == TipoToken.SMENOS) {
+            token = bt();
+            termo();
+        }
+    }
+
+    public void termo() {
+        fator();
+        token = bt();
+        while (token.tipoToken == TipoToken.SMULTIPLICACAO || token.tipoToken == TipoToken.SDIVISAO) {
+            token = bt();
+            fator();
+            token = bt();
+        }
+    }
+
+    public void fator() {
+        if (token.tipoToken == TipoToken.SIDENTIFICADOR || token.tipoToken == TipoToken.SNUMERO); else if (token.tipoToken == TipoToken.SABRE_PARENTESIS) {
+            expressao();
+            token = bt();
+            if (token.tipoToken == TipoToken.SFECHA_PARENTESIS); else {
+                System.out.println(") esperado");
+            }
+        }
+
+    }
 }
